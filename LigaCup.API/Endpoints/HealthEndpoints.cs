33f50363ae15c6381a3727dcfd6ab49c9ade96ad
Sezgin Sahin <moyumbnm@hotmail.com>
2026-09-12 09:40:26 +0200

    Added health endpoint

---
+++ LigaCup.API/Endpoints/HealthEndpoints.cs
@@ -0,0 +1,14 @@
+﻿using LigaCup.ApplicationService.Services;
+
+namespace LigaCup.API.Endpoints;
+
+public static class HealthEndpoints
+{
+    public static void MapHealthEndpoints(this IEndpointRouteBuilder app)
+    {
+        var publicApi = app.MapGroup("/api/health").WithTags("Health");
+
+        publicApi.MapGet("/", async (TournamentService service, CancellationToken cancellationToken) =>
+            Results.Ok());
+    }
+}
